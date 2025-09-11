d635b13178d14ed78b1a87f28796e839d46bf415
jatin <jatinpilpia123@gmail.com>
2025-09-11 10:37:59 +0530

    First commit

---
+++ scripts/checkout.js
@@ -0,0 +1,72 @@
+import {renderOrderSummary} from "./checkout/orderSummary.js";
+import {renderPaymentSummary} from "./checkout/paymentSummary.js";
+import {loadProducts, loadProductsFetch} from "../data/products.js";
+import {loadCart} from "../data/cart.js";
+// import '../data/cart-class.js';
+// import '../data/backend-practice.js';
+
+async function loadPage(){
+   try{
+      // throw 'error1';
+      await loadProductsFetch();
+
+      const value = await new Promise((resolve, reject) => {
+         // throw 'error2';
+         loadCart(() => {
+            reject('error3');
+            // resolve('value1');
+         });
+      });
+   } catch(error){
+      console.log('unexpected Error. Try Again!');
+   }   
+
+   renderOrderSummary();
+   renderPaymentSummary();
+}
+loadPage();
+
+/*
+Promise.all([
+   loadProductsFetch(),
+   new Promise((resolve) => {
+      loadCart(() => {
+         resolve();
+      });
+   })
+
+]).then((values) => {
+   console.log(values);
+   renderOrderSummary();
+   renderPaymentSummary();
+});
+*/
+
+/*
+new Promise((resolve) => {
+   loadProducts(() => {
+      resolve();
+   });
+
+}).then(() => {
+   return new Promise((resolve) => {
+      loadCart(() => {
+         resolve();
+      });
+   });
+
+}).then(() => {
+   renderOrderSummary();
+   renderPaymentSummary();
+});
+*/
+
+/*
+loadProducts(() => {
+   loadCart(() => {
+      renderOrderSummary();
+      renderPaymentSummary();
+   });
+});
+
+*/
